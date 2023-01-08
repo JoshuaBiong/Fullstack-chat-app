aa439efcc6d66d8f939d5c7913dbf10fd5a978e8
JoshBiong <joshuabiong7@gmail.com>
2023-01-08 14:35:30 +0800

    log in and log out secure session done get the solution from a dream

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React , {useEffect}from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
-    
+
 function Chat() {
+
+
+    const logout =()=>{
+localStorage.clear();
+navigate("/login")
+    }
+
+const navigate =useNavigate();
+ 
+useEffect(()=>{
+if(localStorage.getItem("chat-app-user") ===null){
+    navigate("/login")
+}
+
+},[])
+    
+
+
+
+
+
   return (
     <div>Chat
-{/* <a href="/login" onClick={logout}>Logout</a> */}
+<button onClick={logout}>logout</button>
     </div>
     
   )
